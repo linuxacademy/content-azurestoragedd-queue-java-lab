5cc5bc2e72778914c99dd25ae5ef8437dba968b0
Will Boyd <shazzenbot@gmail.com>
2020-01-16 11:37:57 -0500

    end state first pass

--- src/main/java/com/linuxacademy/azurestoragedd/queue/Consumer.java
+++ src/main/java/com/linuxacademy/azurestoragedd/queue/Consumer.java
@@ -1,9 +1,40 @@
 package com.linuxacademy.azurestoragedd.queue;
 
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.queue.*;
+
 public class Consumer {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final String queueName = "myqueue";
+        // Create the connection string for the storage account.
+        final String storageConnectionString = "DefaultEndpointsProtocol=http;" +
+            "AccountName=webconfigtvb7zm6l4lloo;" +
+            "AccountKey=***REMOVED***";
+        try {
+            //Retrieve the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+
+            // Instantiate a queue client.
+            CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
+
+            // Get a reference to the queue.
+            CloudQueue queue = queueClient.getQueueReference(queueName);
+
+            // Process all messages in the queue until no messages remain.
+            CloudQueueMessage retrievedMessage;
+            do {
+                // Retrieve the next available message in the queue.
+                retrievedMessage = queue.retrieveMessage();
+                if (retrievedMessage != null) {
+                    // Process and delete the message.
+                    System.out.println(retrievedMessage.getId() + ":" + retrievedMessage.getMessageContentAsString());
+                    queue.deleteMessage(retrievedMessage);
+                }
+            } while (retrievedMessage != null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/com/linuxacademy/azurestoragedd/queue/Producer.java
+++ src/main/java/com/linuxacademy/azurestoragedd/queue/Producer.java
@@ -1,9 +1,33 @@
 package com.linuxacademy.azurestoragedd.queue;
 
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.queue.*;
+
 public class Producer {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final String queueName = "myqueue";
+        // Create the connection string for the storage account.
+        final String storageConnectionString = "DefaultEndpointsProtocol=http;" +
+            "AccountName=webconfigtvb7zm6l4lloo;" +
+            "AccountKey=***REMOVED***";
+        try {
+            //Retrieve the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+
+            // Instantiate a queue client.
+            CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
+
+            // Get a reference to the queue, and create it if it does not already exist.
+            CloudQueue queue = queueClient.getQueueReference(queueName);
+            queue.createIfNotExists();
+
+            // Add a new message to the queue.
+            CloudQueueMessage message = new CloudQueueMessage("Hello, World!");
+            queue.addMessage(message);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
